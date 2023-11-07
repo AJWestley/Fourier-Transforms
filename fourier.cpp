4c9abd199b77655bc353ea23c8e56b62e485e92a
AJ <alexanderjwestley@gmail.com>
2023-11-07 16:59:51 +0200

    put main functions into fourier namespace

--- fourier.cpp
+++ fourier.cpp
@@ -16,9 +16,9 @@ int main() {
     auto start = std::chrono::high_resolution_clock::now();
 
     printVector(v);
-    fft(v);
+    fourier::fft(v);
     printVector(v);
-    fft(v, true);
+    fourier::fft(v, true);
     printVector(v);
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -27,117 +27,120 @@ int main() {
     return EXIT_SUCCESS;
 }
 
-/**
- * @brief Perform a Fast Fourier Transform (FFT) or Inverse FFT (IFFT) on a complex-valued vector.
- *
- * This function computes the FFT or IFFT of a complex-valued vector, using the radix-2 algorithm
- * when the vector size is a power of 2. If the vector size is not a power of 2, it automatically
- * pads the vector with zeros to the nearest power of 2. It can handle both the forward and
- * inverse transformations based on the 'inverse' parameter.
- *
- * @param v The input complex vector for which the FFT or IFFT is computed.
- * @param inverse If set to true, computes the Inverse FFT (IFFT); otherwise, computes the FFT.
- *
- * @return The transformed complex vector (FFT or IFFT result).
- * 
- * @throws std::invalid_argument If the input vector is empty.
- */
-void fft(complex_vector& v, bool inverse /*= false*/) {
-    int n = v.size();
+namespace fourier {
+    /**
+     * @brief Perform a Fast Fourier Transform (FFT) or Inverse FFT (IFFT) on a complex-valued vector.
+     *
+     * This function computes the FFT or IFFT of a complex-valued vector, using the radix-2 algorithm
+     * when the vector size is a power of 2. If the vector size is not a power of 2, it automatically
+     * pads the vector with zeros to the nearest power of 2. It can handle both the forward and
+     * inverse transformations based on the 'inverse' parameter.
+     *
+     * @param v The input complex vector for which the FFT or IFFT is computed.
+     * @param inverse If set to true, computes the Inverse FFT (IFFT); otherwise, computes the FFT.
+     *
+     * @return The transformed complex vector (FFT or IFFT result).
+     * 
+     * @throws std::invalid_argument If the input vector is empty.
+     */
+    void fft(complex_vector& v, bool inverse /*= false*/) {
+        int n = v.size();
 
-    if (n == 0) {
-        throw std::invalid_argument("Input vector is empty.");
-    }
-
-    // Ideal case - vector size is a power of 2
-    if (isPowerOf2(n)) {
-        fftRadix2(v, inverse);
-        return;
-    }
-    
-    // Non-ideal case - vector size not a power of 2
-    v.resize(nextPowerOf2(n), 0);
-    fftRadix2(v, inverse);
-}
-
-/**
- * @brief Perform a Fast Fourier Transform (FFT) or Inverse FFT (IFFT) using the radix-2 algorithm.
- *
- * This function computes the FFT or IFFT of a complex-valued vector using the radix-2 algorithm.
- * It can handle both the forward and inverse transformations based on the 'inverse' parameter.
- *
- * @param v The input complex vector for which the FFT or IFFT is computed.
- * @param inverse If set to true, computes the Inverse FFT (IFFT); otherwise, computes the FFT.
- *
- * @return The transformed complex vector (FFT or IFFT result).
- *
- * @throws std::invalid_argument If the length of the input vector 'v' is not a power of 2, an exception is thrown.
- */
-void fftRadix2(complex_vector& v, bool inverse /*= false*/) {
-    // Base case
-    int n = v.size();
-    if (n == 1) {
-        return;
-    }
-    
-    // Error check before run
-    if (nextPowerOf2(n) != n) {
-        throw std::invalid_argument("Length input vector not a power of 2.");
-    }
-
-    // Permute to get odd and even half-vectors
-    complex_vector vEven(n / 2);
-    complex_vector vOdd(n / 2);
-    for (int i = 0; i < n/2; i++) {
-        vEven[i] = v[2*i];
-        vOdd[i] = v[2*i+1];
-    }
-
-    // Recursively calculate sub-vectors
-    fftRadix2(vEven, inverse);
-    fftRadix2(vOdd, inverse);
-
-    // Combine to get full vector
-    combineHalfVectors(v, vEven, vOdd, n, inverse);
-}
-
-/**
- * @brief Generates a Fourier matrix of size N.
- *
- * This function generates a square Fourier matrix of size N, containing complex values.
- *
- * @param N The size of the square Fourier matrix to generate. N must be a positive integer.
- * @param inverse If set to true, computes the inverse Fourier matrix; otherwise, computes the standard Fourier matrix.
- *
- * @return A square Fourier matrix of size N filled with complex values.
- *
- * @throws std::invalid_argument If N is not a positive integer, an exception is thrown.
- */
-complex_matrix fourierMatrix(int N, bool inverse /*= false*/) {
-    // N must be positive
-    if (N <= 0) {
-        throw std::invalid_argument("N must be a positive integer");
-    }
-
-    // Initialize Fourier matrix to all 1
-    complex_matrix F(N, complex_vector(N, 1.0));
-
-    // Compute fourier coefficient w
-    std::complex<double> w = exp((D_PI * I) / (double) N);
-    if (inverse) w = pow(w, -1);
-
-    // Populate matrix
-    # pragma omp parallel for
-    for (int i = 1; i < N; i++) {
-        for (int j = 1; j < N; j++) {
-            F[i][j] = std::complex<double>(pow(w, i*j));
+        if (n == 0) {
+            throw std::invalid_argument("Input vector is empty.");
         }
+
+        // Ideal case - vector size is a power of 2
+        if (isPowerOf2(n)) {
+            fftRadix2(v, inverse);
+            return;
+        }
+        
+        // Non-ideal case - vector size not a power of 2
+        v.resize(nextPowerOf2(n), 0);
+        fftRadix2(v, inverse);
     }
-    return F;
+
+    /**
+     * @brief Perform a Fast Fourier Transform (FFT) or Inverse FFT (IFFT) using the radix-2 algorithm.
+     *
+     * This function computes the FFT or IFFT of a complex-valued vector using the radix-2 algorithm.
+     * It can handle both the forward and inverse transformations based on the 'inverse' parameter.
+     *
+     * @param v The input complex vector for which the FFT or IFFT is computed.
+     * @param inverse If set to true, computes the Inverse FFT (IFFT); otherwise, computes the FFT.
+     *
+     * @return The transformed complex vector (FFT or IFFT result).
+     *
+     * @throws std::invalid_argument If the length of the input vector 'v' is not a power of 2, an exception is thrown.
+     */
+    void fftRadix2(complex_vector& v, bool inverse /*= false*/) {
+        // Base case
+        int n = v.size();
+        if (n == 1) {
+            return;
+        }
+        
+        // Error check before run
+        if (nextPowerOf2(n) != n) {
+            throw std::invalid_argument("Length input vector not a power of 2.");
+        }
+
+        // Permute to get odd and even half-vectors
+        complex_vector vEven(n / 2);
+        complex_vector vOdd(n / 2);
+        for (int i = 0; i < n/2; i++) {
+            vEven[i] = v[2*i];
+            vOdd[i] = v[2*i+1];
+        }
+
+        // Recursively calculate sub-vectors
+        fftRadix2(vEven, inverse);
+        fftRadix2(vOdd, inverse);
+
+        // Combine to get full vector
+        combineHalfVectors(v, vEven, vOdd, n, inverse);
+    }
+
+    /**
+     * @brief Generates a Fourier matrix of size N.
+     *
+     * This function generates a square Fourier matrix of size N, containing complex values.
+     *
+     * @param N The size of the square Fourier matrix to generate. N must be a positive integer.
+     * @param inverse If set to true, computes the inverse Fourier matrix; otherwise, computes the standard Fourier matrix.
+     *
+     * @return A square Fourier matrix of size N filled with complex values.
+     *
+     * @throws std::invalid_argument If N is not a positive integer, an exception is thrown.
+     */
+    complex_matrix fourierMatrix(int N, bool inverse /*= false*/) {
+        // N must be positive
+        if (N <= 0) {
+            throw std::invalid_argument("N must be a positive integer");
+        }
+
+        // Initialize Fourier matrix to all 1
+        complex_matrix F(N, complex_vector(N, 1.0));
+
+        // Compute fourier coefficient w
+        std::complex<double> w = exp((D_PI * I) / (double) N);
+        if (inverse) w = pow(w, -1);
+
+        // Populate matrix
+        # pragma omp parallel for
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                F[i][j] = std::complex<double>(pow(w, i*j));
+            }
+        }
+        return F;
+    }
 }
 
 /**
  * Combine the odd and even half-vectors during the FFT.
+ * Performs the combination in parallel if the vectors are large enough.
  */
 static void combineHalfVectors(complex_vector& v, const complex_vector& vEven, const complex_vector& vOdd, int n, int inverse) {
     double angle = D_PI / n * (inverse ? 1 : -1);
--- fourier.h
+++ fourier.h
@@ -4,9 +4,12 @@
 #define complex_vector std::vector<std::complex<double>>
 #define complex_matrix std::vector<std::vector<std::complex<double>>>
 
-void fft(complex_vector& v, bool inverse = false);
-void fftRadix2(complex_vector& v, bool inverse = false);
-complex_matrix fourierMatrix(int N, bool inverse = false);
+namespace fourier {
+    void fft(complex_vector& v, bool inverse = false);
+    void fftRadix2(complex_vector& v, bool inverse = false);
+    complex_matrix fourierMatrix(int N, bool inverse = false);
+}
+
 static void combineHalfVectors(complex_vector& v, const complex_vector& vEven, const complex_vector& vOdd, int n, int inverse);
 static void printVector(const complex_vector& v);
 static void printMatrix(const complex_matrix& M);
